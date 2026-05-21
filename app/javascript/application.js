dfb62fb0de86059181c6546aa283afbb211ee777
Cindy Eude <eudecindy@gmail.com>
2026-05-21 11:38:25 +0200

    save chargement

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,11 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+//= link_tree ../images
+//= link_directory ../stylesheets .css
+//= link_tree ../../javascript .js
+//= link popper.js
+//= link bootstrap.min.js
+
 import "@hotwired/turbo-rails"
 import "controllers"
+import "@popperjs/core";
+import "bootstrap";
